ad5b615f9c13753620a0874f858e54040cf3fb1c
MrOffer <thomas.e.offer@gmail.com>
2019-11-20 11:55:36 -0600

    finished types exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,38 @@
+
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello, World!");
-        }
+
+        int myFavoriteNumber = 1111;
+        System.out.print(myFavoriteNumber);
+
+        String myString = "Yay!";
+        System.out.print(myString);
+
+        double myNumber = 3.14;
+        System.out.print(myNumber);
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        int y = 5;
+        System.out.println(++y);
+        System.out.println(y);
+
+        int z = 4;
+        z += 5;
+
+        int a = 3;
+        int b = 4;
+        b *= a;
+
+        int c = 10;
+        int d = 2;
+         c /= d;
+        d -= c;
+
+
+}
 }

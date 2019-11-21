73d63cc1852dada5cb66d3e06446f8377751968a
MrOffer <thomas.e.offer@gmail.com>
2019-11-21 14:28:29 -0600

    finished fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,10 +2,18 @@ import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        for (Long i = 2L; i <= 1000000; i *= i) {
-            System.out.print(i+"\n");
-        }
+        for (int i = 0; i <= 100; i++) {
+            if (i % 15 == 0) {
+                System.out.print("FizzBuzz\n");
+            } else if (i % 3 == 0) {
+                System.out.print("Fizz \n");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz \n");
+            } else {
+                System.out.print(i + "\n");
+            }
 
+        }
     }
 }
 

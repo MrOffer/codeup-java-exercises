2991ec9e0d0f1f2b89d178dc9651f2b5127a3f44
MrOffer <thomas.e.offer@gmail.com>
2019-11-21 14:22:52 -0600

    second for loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        for (Integer i = 100; i >= -10; i -= 5) {
+        for (Long i = 2L; i <= 1000000; i *= i) {
             System.out.print(i+"\n");
         }
 

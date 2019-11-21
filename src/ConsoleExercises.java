4200fc9657687e7a1da9d82de6dfd2943a7d8bb8
MrOffer <thomas.e.offer@gmail.com>
2019-11-21 11:44:43 -0600

    finished consoleexercises walk through

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,19 +3,35 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
+
         System.out.printf("The value of pi is approximately %.2f %n", pi);
-
-
 
         Scanner scanner = new Scanner(System.in);
 
         scanner.useDelimiter("\n");
 
+        System.out.println("give me a number");
+
+        int num = scanner.nextInt();
+
+        System.out.print("you entered: " + num );
+
+
+        System.out.println("give me three words");
+
+        String str1 = scanner.next();
+        String str2 = scanner.next();
+        String str3 = scanner.next();
+
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+
         System.out.print("Please, enter a sentence: ");
 
         String userInput = scanner.nextLine();
 
-        System.out.println("The sentence you entered is: \"" + userInput + "\"");
+        System.out.println("The sentence you entered is: " + userInput);
 
 
         System.out.println("Enter the length of the classroom:");
--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,9 +3,17 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
         int i = 5;
         while (i <= 15) {
-            System.out.print(i +" ");
+            System.out.print(i + " ");
             i++;
         }
+//            do {
+//                int a = 0;
+//                System.out.print(a +"/n");
+//                a++;
+//
+//            }while (a < 100);
+//
+//        }
 
         }
 }

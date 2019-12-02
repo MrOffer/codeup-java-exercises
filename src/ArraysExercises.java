5b9182a37cc24d064a030c0fedbe3e8a4b4d10ca
MrOffer <thomas.e.offer@gmail.com>
2019-12-02 11:01:10 -0600

    finished arrays exercise

---
+++ src/ArraysExercises.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class ArraysExercises {
+
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
+        // using the array initializer syntax
+
+        String[] People = {"John", "Paul", "Ringo"};
+
+        for (String Person : People) {
+            System.out.println(Person);
+        }
+
+        System.out.println(Arrays.toString(addPerson(People, "George")));
+    }
+    public static String[] addPerson (String[]People, String Person) {
+        String[] People2 = Arrays.copyOf(People, 4);
+        People2[3] = Person;
+        return People2;
+    }
+
+}

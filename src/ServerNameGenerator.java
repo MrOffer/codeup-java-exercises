93d0d4171de87e4aabd0e3446e86e892603325fa
MrOffer <thomas.e.offer@gmail.com>
2019-12-02 11:31:33 -0600

    finished server name generator

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,24 @@
+public class ServerNameGenerator {
+
+
+     public static String[] adjectives = {"big", "small", "average", "short", "tall", "jumbo", "puny", "wide", "narrow", "substantial"};
+     public static String[] nouns = {"boy", "girl", "cat", "dog", "elephant", "deer", "you", "man", "woman", "monkey"};
+
+
+        public static String randomAdjective(){
+            int randomAdj = (int)Math.floor(Math.random() * adjectives.length);
+            return adjectives[randomAdj];
+        }
+        public static String randomNoun(){
+            int randomN = (int)Math.floor(Math.random() * nouns.length);
+            return nouns[randomN];
+        }
+
+        public static void main(String[] args) {
+            System.out.println( randomAdjective()+ " - " +randomNoun());
+        }
+
+        }
+
+
+

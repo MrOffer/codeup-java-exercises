994c94f788e6873eaa2c3c046fdb49f8c6e55781
MrOffer <thomas.e.offer@gmail.com>
2019-12-03 10:38:30 -0600

    finished square and rectangle exercise

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -4,6 +4,19 @@ public class Rectangle {
     protected int length;
     protected int width;
 
+    public Rectangle(int Length, int Width){
+        length = Length;
+        width = Width;
+    }
+
+    public int getLength(){
+        return length;
+    }
+
+    public int getWidth(){
+        return width;
+    }
+
     public int getArea(){
       return length * width;
     }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,7 +1,17 @@
 package shapes;
 
-    public class Square extends Rectangle {
-        public Square(int side) {
-            super(side, side);
-        }
+public class Square extends Rectangle {
+    private int side;
+    public Square(int Side){
+        super(Side,Side);
+        side = Side;
+    }
+
+    public int getArea(){
+        return 4*side;
+    }
+
+    public int getPerimeter(){
+        return side*side;
+    }
 }

f916cf506eaf437a9239b0c34549dd6fc18a72bf
MrOffer <thomas.e.offer@gmail.com>
2019-12-04 12:30:48 -0600

    setup for collections exercises

---
+++ src/grades/Student.java
@@ -0,0 +1,16 @@
+package grades;
+
+import java.util.ArrayList;
+
+public class Student {
+    private String name;
+
+    ArrayList<Integer> grades = new ArrayList<>();
+
+//    // returns the student's name
+//    public String getName();
+//    // adds the given grade to the grades property
+//    public void addGrade(int grade);
+//    // returns the average of the students grades
+//    public double getGradeAverage();
+}
